0006d270970fbf76988eb677b58a253f056e0f4c
mahdi montazeri <mahdi@gmail.com>
2023-08-21 16:58:15 +0330

    add tickets to database

--- Gold.Core/Domain/Entities/Ticket/Ticket.cs
+++ Gold.Core/Domain/Entities/Ticket/Ticket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,12 @@ namespace Gold.Core.Domain.Entities.Ticket
 {
     public class Ticket
     {
+        [Key]
         public string TicketId { get; set; } = Guid.NewGuid().ToString();       
         public Status StatusEnum { get; set; }
 
         //nav props
-        public IEnumerable<Ticket> Messages { get; set; }
+        public IEnumerable<TicketMessage> Messages { get; set; }
 
         public Ticket()
         {            
